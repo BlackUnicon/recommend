feda5fff112bbe63e6b32e0500ad1ec236fa4bf5
LongNhat <ndlongnhat@gmail.com>
2022-07-14 23:52:37 +0700

    C6

--- train.py
+++ train.py
@@ -5,25 +5,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 import re
 
-# def standardize_data(row):
-#     row = re.sub(r"[\.,\?]+$-", "", row)
-#     row = row.replace(",", " ").replace(".", " ") \
-#         .replace(";", " ").replace("“", " ") \
-#         .replace(":", " ").replace("”", " ") \
-#         .replace('"', " ").replace("'", " ") \
-#         .replace("!", " ").replace("?", " ") \
-#         .replace("-", " ").replace("?", " ")
-#     row = row.strip().lower()
-#     return row
-
-# uri = ""
-# client = MongoClient(uri)
-# db = client[""]
-# collection = db[""]
-# results = collection.find({})
-
 def get_recommendations(id):
-
     idx = indices[id]
     sim_scores = list(enumerate(cosine_sim[idx]))
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
@@ -32,13 +14,17 @@ def get_recommendations(id):
     return data['title'].iloc[movie_indices]
 
 data = pd.read_csv("data_test.csv")
-id = '62b6c83b023830051e30e449'
-data['bodyFull'] = data['bodyFull'].fillna('')
-tfidf = TfidfVectorizer(stop_words='english')
-tfidf_matrix = tfidf.fit_transform(data['bodyFull'])
-cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+id = '62b6d6e1a341c48fa5a3ffd4'
 
+data['sum'] = data['title'] + data['bodyFull'] + data['keyWords'] + data['body']
+data['sum'] = data['sum'].astype(str).str.replace(r"W","")
+data['sum'] = data['sum'].fillna('')
+
+tfidf = TfidfVectorizer(stop_words='english')
+tfidf_matrix = tfidf.fit_transform(data['sum'])
+cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
 indices = pd.Series(data.index, index=data['_id']).drop_duplicates()
+
 print(get_recommendations(id))
 print(data[data['_id'] == id]['title'])
 
